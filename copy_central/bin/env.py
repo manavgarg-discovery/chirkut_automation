b760c2a08811ecf6ef848297669935aedada853d
manavgarg-discovery <manav.garg@vitalityglobal.com>
2024-10-18 16:50:27 +0530

    add env.py

---
+++ copy_central/bin/env.py
@@ -0,0 +1,6 @@
+class Env:
+    github_token = ""
+    ticket_number = "1234456"
+    remote_repo_name = "v1-gutenberg-central-app-flutter"
+    remote_file_path = "pubspec.yaml"
+    output_file_path = "bin/central_pubspec.yaml"
